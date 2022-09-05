ff774f7b2606bff116f3342b75e06cd4f278548a
waku1044 <walterg610@gmail.com>
2022-09-04 23:23:16 -0300

    slide y comentario funcionando

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -11,9 +11,9 @@ function Slide() {
     const imgCenter = useRef(null);
     const imgRight = useRef(null);
     let [count, setCount] = useState(null);
-
+    let [descrip, setDescrip] = useState('Esta es mi casa');
   
-   
+ 
 
     
     
@@ -24,18 +24,21 @@ function Slide() {
             case -1:
                 imgLeft.current.src = img.img2;
                 imgCenter.current.src = img.img3;
+                setDescrip('Esta es la casa de un vecino');
                 imgRight.current.src = img.img1;
                 console.log('case1 ' + count );
                 break;
             case -2:
                 imgLeft.current.src = img.img3;
                 imgCenter.current.src = img.img1;
+                setDescrip('Casa de otro vecino');
                 imgRight.current.src = img.img2;
                 console.log('case2 ' + count );
                 break;
             default:
                 imgLeft.current.src = img.img1;
                 imgCenter.current.src = img.img2;
+                setDescrip('Esta es mi casa');
                 imgRight.current.src = img.img3;
                 console.log('default ' + count )
                 setCount(null);
@@ -51,36 +54,43 @@ function Slide() {
             case 1:
                 imgLeft.current.src = img.img3;
                 imgCenter.current.src = img.img1;
+                setDescrip('Casa de otro vecino');
                 imgRight.current.src = img.img2;
                 console.log('case1 ' + count );
                 break;
             case 2:
                 imgLeft.current.src = img.img2;
-                imgCenter.current.src = img.img3;
+                imgCenter.current.src = img.img3;   
+                setDescrip('Esta es la casa de un vecino');
                 imgRight.current.src = img.img1;
                 console.log('case2 ' + count );
                 break;
             default:
                 imgLeft.current.src = img.img1;
                 imgCenter.current.src = img.img2;
+                setDescrip('Esta es mi casa');
                 imgRight.current.src = img.img3;
                 console.log('default ' + count )
                 setCount(null);
                 break;
         }
     }
+{/* setInterval(next, 4000); */ }
     return ( 
         <>
-        <div className="row mt-3">
-	            <div className="slider">
-	                <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" id='fle-left' onClick={prev} viewBox="0 0 512 512"><title>Caret Back</title><path d="M321.94 98L158.82 237.78a24 24 0 000 36.44L321.94 414c15.57 13.34 39.62 2.28 39.62-18.22v-279.6c0-20.5-24.05-31.56-39.62-18.18z"/></svg>
-	                <img src={img.img1} className='h-50' id="imgleft" ref={imgLeft} alt='imgleft' />
-	                <img src={img.img2} className="h-75"  id="imgcenter" ref={imgCenter} alt='imgcenter' />
-	                <img src={img.img3} className="h-50" id="imgright" ref={imgRight} alt='imgright' />
-	                <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" id='fle-right' onClick={next}  viewBox="0 0 512 512"><title>Caret Back</title><path d="M321.94 98L158.82 237.78a24 24 0 000 36.44L321.94 414c15.57 13.34 39.62 2.28 39.62-18.22v-279.6c0-20.5-24.05-31.56-39.62-18.18z"/></svg>
-	            </div>
-	        </div> 
-            </>
+            <div className="row mt-3">
+    	            <div className="slider">
+    	                <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" id='fle-left' onClick={prev} viewBox="0 0 512 512"><title>Caret Back</title><path d="M321.94 98L158.82 237.78a24 24 0 000 36.44L321.94 414c15.57 13.34 39.62 2.28 39.62-18.22v-279.6c0-20.5-24.05-31.56-39.62-18.18z"/></svg>
+    	                <img src={img.img1} className='h-50' id="imgleft" ref={imgLeft} alt='imgleft' />
+    	                <img src={img.img2} className="h-75"  id="imgcenter" ref={imgCenter} alt='imgcenter' />
+    	                <img src={img.img3} className="h-50" id="imgright" ref={imgRight} alt='imgright' />
+    	                <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" id='fle-right' onClick={next}  viewBox="0 0 512 512"><title>Caret Back</title><path d="M321.94 98L158.82 237.78a24 24 0 000 36.44L321.94 414c15.57 13.34 39.62 2.28 39.62-18.22v-279.6c0-20.5-24.05-31.56-39.62-18.18z"/></svg>
+                        <div className='align-self-end' id='coment'>
+                            <h5>{ descrip } </h5>
+    	                </div>
+    	           </div> 
+            </div>
+        </>
             )
 
     }
