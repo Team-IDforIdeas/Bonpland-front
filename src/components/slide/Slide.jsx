4a2a3b22a887d2fbcaedd1e20d36509f63e0f528
waku1044 <walterg610@gmail.com>
2022-09-06 09:51:51 -0300

    agregado de hr en comentario

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -10,7 +10,7 @@ function Slide() {
     const imgLeft = useRef(null);
     const imgCenter = useRef(null);
     const imgRight = useRef(null);
-    let [count, setCount] = useState(null);
+    let [count, setCount] = useState(0);
     let [descrip, setDescrip] = useState('Esta es mi casa');
   
  
@@ -75,7 +75,7 @@ function Slide() {
                 break;
         }
     }
-{/* setInterval(next, 4000); */ }
+ {/* setInterval(next, 4000)*/ }
     return ( 
         <>
             <div className="row mt-3">
@@ -85,8 +85,9 @@ function Slide() {
     	                <img src={img.img2} className="h-75"  id="imgcenter" ref={imgCenter} alt='imgcenter' />
     	                <img src={img.img3} className="h-50" id="imgright" ref={imgRight} alt='imgright' />
     	                <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" id='fle-right' onClick={next}  viewBox="0 0 512 512"><title>Caret Back</title><path d="M321.94 98L158.82 237.78a24 24 0 000 36.44L321.94 414c15.57 13.34 39.62 2.28 39.62-18.22v-279.6c0-20.5-24.05-31.56-39.62-18.18z"/></svg>
-                        <div className='align-self-end' id='coment'>
+                        <div className='align-self-end mt-5' id='coment'>
                             <h5>{ descrip } </h5>
+                        <hr/>
     	                </div>
     	           </div> 
             </div>
